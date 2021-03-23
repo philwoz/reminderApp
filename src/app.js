2d4dbe1ec372412482878434713326c5997a19fb
philwoz <61164019+philwoz@users.noreply.github.com>
2021-03-23 13:58:47 +0000

    yarn

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
 console.log(process.argv)
+
+const { addNote} = require("../utils/notes")
+const yargs = require("yargs")
+console.log(yargs.argv)
+const command = process.argv[2]
+
+if (command == "add") {
+    console.log("adding a note")
+    addNote(yargs.argv.note)
+} else if (command == "remove"){
+    console.log("removing a note")
+} else if (command == "list"){
+    console.log("listing notes")
+} else {
+    console.log("command not recognised")
+}
+
+
+
+
+// console.log(process.argv)
+
+// if (process.argv[2] == "phil") {
+//     console.log("thats my name")   
+// } else {
+//     console.log("thats my not name") 
+// }
+
---
+++ utils/notes.js
@@ -0,0 +1,7 @@
+const addNote = (myNote) => {
+    console.log(myNote)
+}
+
+module.exports = {
+    addNote,
+}
