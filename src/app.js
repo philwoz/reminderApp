4614a2fee13a1732ce365895fe88a3e7b1a4d70d
philwoz <61164019+philwoz@users.noreply.github.com>
2021-03-23 15:49:40 +0000

    new commit

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
 console.log(process.argv)
 
-const { addNote} = require("../utils/notes")
+const { addNote, listNotes, removeNote} = require("../utils/notes")
 const yargs = require("yargs")
 console.log(yargs.argv)
 const command = process.argv[2]
 
 if (command == "add") {
-    console.log("adding a note")
+    console.log("adding a note....")
     addNote(yargs.argv.note)
 } else if (command == "remove"){
-    console.log("removing a note")
+    console.log("deleting note....")
+    removeNote(yargs.argv.note)
 } else if (command == "list"){
-    console.log("listing notes")
+    listNotes()
 } else {
     console.log("command not recognised")
 }
--- utils/notes.js
+++ utils/notes.js
@@ -1,7 +1,49 @@
-const addNote = (myNote) => {
-    console.log(myNote)
+const fs = require("fs")
+
+const loadNotes = () => {
+    try{
+        const dataBuffer = fs.readFileSync("src/notes.json")
+        const notesJson = dataBuffer.toString()
+        return JSON.parse(notesJson)
+    } catch (error) {
+        return []
+    }
 }
+
+const saveNotes = allNotes => {
+    const notesJson = JSON.stringify(allNotes)
+    fs.writeFileSync("src/notes.json", notesJson)
+}
+
+const addNote = (myNote) => {
+    const allNotes = loadNotes()
+    allNotes.push({reminder: myNote})
+    
+    saveNotes(allNotes)
+}
+
+const listNotes = () => {
+    const allNotes = loadNotes()
+
+    allNotes.map(note => {
+        console.log(note)
+    })
+}
+
+const removeNote = noteToDelete => {
+    const allNotes = loadNotes()
+
+    const notesToKeep = allNotes.filter( note => {
+        return note.reminder != noteToDelete
+    })
+
+    saveNotes(notesToKeep)
+}
+
 
 module.exports = {
     addNote,
+    listNotes,
+    removeNote,
 }
+
